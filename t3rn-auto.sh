5aa15ccb7873b968702080753a77a82fc3e4c978
phoenixit99 <hoanguit@gmail.com>
2024-11-06 11:08:43 +0700

    Update t3rn-auto.sh

--- t3rn-auto.sh
+++ t3rn-auto.sh
@@ -37,8 +37,8 @@ mkdir t3rn
 cd t3rn 
 
 # Download and extract executor
-wget https://github.com/t3rn/executor-release/releases/download/v0.21.11/executor-linux-v0.21.11.tar.gz
-tar -xf executor-linux-v0.21.11.tar.gz
+wget https://github.com/t3rn/executor-release/releases/download/v0.22.0/executor-linux-v0.22.0.tar.gz
+tar -xf executor-linux-v0.22.0.tar.gz
 
 # Prompt user for private key
 
